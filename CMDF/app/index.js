345dd8ccafebe0d0cdafab4628b08cd5bf8024f2
Devina Prasad Jaiswal <43189092+devinapj@users.noreply.github.com>
2019-03-10 09:37:50 -0700

    removed socket programming

--- CMDF/app/index.js
+++ CMDF/app/index.js
@@ -2,41 +2,12 @@ import { geolocation } from "geolocation";
 import document from "document";
 // Import the messaging module
 import * as messaging from "messaging";
-
-// Listen for the onopen event
-messaging.peerSocket.onopen = function() {
-  // Ready to send or receive messages
-}
-// Listen for the onerror event
-messaging.peerSocket.onerror = function(err) {
-  // Handle any errors
-  console.log("Connection error: " + err.code + " - " + err.message);
-}
-// Send a message to the peer
-function sendMessage() {
-  // Sample data
-  var data = {
-    title: 'My test data',
-    isTest: true,
-    records: [1, 2, 3, 4],
-   // Latitude: position.coords.latitude,
-    //Longitude: position.coords.longitude
-  }
-
-  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
-    // Send the data to peer as a message
-    messaging.peerSocket.send(data);
-  }
-}
-
-//geolocation.getCurrentPosition(locationSuccess, locationError);
+import * as fs from "fs";
 
 function locationSuccess(position) {
     console.log("Latitude: " + position.coords.latitude,
                 "Longitude: " + position.coords.longitude);
-  var demotext = document.getElementById("demotext");
 }
-
 
 function locationError(error) {
   console.log("Error: " + error.code,
@@ -45,8 +16,6 @@ function locationError(error) {
 
 let mybutton = document.getElementById("mybutton");
 mybutton.onactivate = function(evt) {
-  console.log("CLICKED!");
+  console.log("Location Sent!");
   geolocation.getCurrentPosition(locationSuccess, locationError);
-  sendMessage();
-
 }

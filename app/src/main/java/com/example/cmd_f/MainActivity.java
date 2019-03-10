65770e94a7370d4d7a89dda077cae0bf7a196a33
Kathryn Ng <ng.kathryn@gmail.com>
2019-03-10 03:20:15 -0700

    edited print string

--- app/src/main/java/com/example/cmd_f/MainActivity.java
+++ app/src/main/java/com/example/cmd_f/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.cmd_f;
 
+import android.content.Context;
+import android.location.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.location.Geocoder;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,19 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.textView);
+        try {
+            tv.setText(geocode(this));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
-    public void geocode()throws IOException{
-    Geocoder gc = new Geocoder(context);
-if(gc.isPresent()) {
-    List<address> list = gc.getFromLocation(37.42279, -122.08506, 1);
-    Address address = list.get(0);
-    StringBuffer str = new StringBuffer();
-    str.append("Name: "+address.getLocality() + '\n');
-    str.append("Sub - Admin Ares: "+address.getSubAdminArea() + '\n');
-    str.append("Admin Area: "+address.getAdminArea() + '\n');
-    str.append("Country: "+address.getCountryName() + '\n');
-    str.append("Country Code: "+address.getCountryCode() + '\n');
-    String strAddress = str.toString();
-}
+
+    public String geocode(Context context)throws IOException{
+            String str = "";
+        Geocoder gc = new Geocoder(context);
+        if(gc.isPresent()) {
+            List<Address> list = gc.getFromLocation(46.42279, -123.08506, 1);
+            Address address = list.get(0);
+            str += "Name: "+address.getLocality() + '\n';
+            str += "Sub - Admin Ares: "+address.getSubAdminArea() + '\n';
+            str += "Admin Area: "+address.getAdminArea() + '\n';
+            str += "Country: "+address.getCountryName() + '\n';
+            str += "Country Code: "+address.getCountryCode() + '\n';
+
+
+    }
+    return str;
     }
 }
